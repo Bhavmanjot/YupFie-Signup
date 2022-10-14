ba27edd2fd134077621b248677ec757e15164c89
Bhavmanjot <93830159+Bhavmanjot@users.noreply.github.com>
2022-10-14 19:09:43 +0530

    Update signup.js

--- signup.js
+++ signup.js
@@ -1,17 +1,55 @@
-import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-auth.js";
+const signupBtn = document.getElementById("signupbtn");
 
-const auth = getAuth();
-createUserWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed in 
-    const user = userCredential.user;
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // ..
+const txtEmail = document.getElementById('email');
+const txtPassword = document.getElementById('password');
+const userId = document.getElementById('name');
+
+const database = firebase.database();
+const rootRef = database.ref('users');
+const auth = firebase.auth();
+
+signupBtn.addEventListener("click", function(){
+
+    var email = txtEmail.value;
+    var pass = txtPassword.value;
+
+    //Signing up
+    firebase.auth().createUserWithEmailAndPassword(email, pass)
+    .then((data) => {
+
+      //send verification email
+      sendVerificationEmail();
+    }) 
+    .catch(function(error) {
+    // Handle Errors here.
+    //var errorCode = error.code;
+    var errorMessage = error.message;
+    alert("Error :" + errorMessage);
+    });
+
   });
-  $("signupbtn").click(function() {
-    submit();
-  });
+  //verification email function
+  var sendVerificationEmail = () => {
+    auth.currentUser.sendEmailVerification()
+    .then(() => {
+      alert("Verification Email Sent! Check your mailbox.")
+    })
+    .catch(error => {
+      alert("Error :" + errorMessage);
+    })
+  }      
+
+  //DATABASE
+  //'set' adds new data to he db
+  signupBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    
+    let id = data.User.uid;
+
+      function writeUserData(userId, name, email) {
+        firebase.database().ref('Users/' + userId).set({
+          Username: name,
+          Email: email,
+        });
+      }
+  }); 
